a6dc94afb1cb2ab2f27a37dddf587ab5c813e29c
JeanJoblin <invisibleorbs@gmail.com>
2023-05-15 10:47:47 -0400

    Make navbar sliiide

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -26,7 +26,11 @@ function displayNavItems() {
 
 export default function Navbar({open}) {
   return (
-    <div id="sidebar" className={` w-[200px] fixed z-10 right-0 h-full bg-black bg-opacity-70 flex-row backdrop-blur-sm border-l-2 border-orange-500 ${!open && 'w-[0] border-none'} transition`}>
+    <div id="sidebar" 
+    className={
+      `fixed z-10 right-[-200px] h-full bg-black bg-opacity-70 flex-row backdrop-blur-sm border-orange-500 w-[200px] transition border-l-2
+      ${open && `translate-x-[-200px]`}
+    `}>
       {displayNavItems()}
     </div>
   )
